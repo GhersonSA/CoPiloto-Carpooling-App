9735f9047642297548d5f1995361811e856f6a77
Gherson <Gherson2002@outlook.com>
2026-02-10 12:51:12 +0100

    Cambios archivo AndroidManifest.xml

--- copiloto-nest-backend/src/main.ts
+++ copiloto-nest-backend/src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
   console.log(
     `Server running on http://localhost:${process.env.PORT || 4000}`,
   );
